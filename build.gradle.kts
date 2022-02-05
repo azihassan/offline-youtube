ec7d73e60ea1eb86dccf2cd19c02a037a79d338f
Azi Hassan <azi.hassan@live.fr>
2022-02-05 01:33:26 +0100

    [master] Build and run tests on each push

---
+++ build.gradle.kts
@@ -0,0 +1,43 @@
+import org.jetbrains.compose.compose
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.5.21"
+    id("org.jetbrains.compose") version "1.0.0-alpha3"
+    id("jacoco")
+}
+
+group = "me.hassan"
+version = "1.0"
+
+repositories {
+    google()
+    mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+}
+
+dependencies {
+    implementation(compose.desktop.currentOs)
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
+    implementation("org.jsoup:jsoup:1.14.3")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "11"
+}
+
+compose.desktop {
+    application {
+        mainClass = "MainKt"
+        nativeDistributions {
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            packageName = "youtube-offline"
+            packageVersion = "1.0.0"
+        }
+    }
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
